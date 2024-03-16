8102e707eeb83fa6a6890a277535334c4cc0a76e
ccbaxter2735 <erussard624@gmail.com>
2024-03-16 01:19:02 +0100

    creation du fichier projet

---
+++ MedicaFinal/MedicaFinal/MedicaFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedicaFinalApp.swift
+//  MedicaFinal
+//
+//  Created by Apprenant69 on 16/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MedicaFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
